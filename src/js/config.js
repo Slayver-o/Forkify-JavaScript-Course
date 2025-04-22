fe0116a55289b69b5cf05d7ea5ee28bfb89f844b
Slayver-o <mohamed.a.yehia9@gmail.com>
2025-04-22 05:29:54 +0200

    Hello World

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '6d82c288-ee89-4c57-bcab-6a8e30df8659';
+export const MODEL_CLOSE_SEC = 2.5;
